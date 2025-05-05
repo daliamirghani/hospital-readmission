fdc3633a66ddaecd0ea00081c997c86691d75f06
dalia <daliamirghani@gmail.com>
2025-05-06 00:33:00 +0300

    added xgboost model & evaluation function

--- models/xgboostModel.py
+++ models/xgboostModel.py
@@ -1,12 +1,23 @@
 #import necessary libraries (e.g., sklearn, pandas, xgboost, etc.)
-#import and preprocess training data
-#import and preprocess test data
-#apply xgboost-specific preprocessing steps
+from preprocessing.preproc import dataframe
+from xgboost import XGBClassifier
+from scipy.stats import randint, uniform
+from sklearn.model_selection import RandomizedSearchCV
+from helper_functions import evaluate
 
-#train initial xgboost model with default parameters
-#evaluate: accuracy score + confusion matrix
+param_dist = {
+    'n_estimators': randint(2, 100),
+    'max_depth': randint(2, 10),
+    'learning_rate': uniform(0.01, 0.29)
+}
 
-#perform cross-validation and hyperparameter tuning
-#train final model with best hyperparameters
+#pretend we have x_test,y_test, x_train, y_train
+xgb = XGBClassifier(objective='binary:logistic', use_label_encoder=False, eval_metric='logloss')
+xgb = RandomizedSearchCV(xgb, param_dist, cv=5)
+xgb.fit(x_train, y_train)
+prediction = xgb.best_estimator_.predict(x_test)
+prediction_prob = xgb.best_estimator_.predict_proba(x_test)[:, 1]
+evaluate(y_test,prediction, prediction_prob)
 
-#evaluate final model on test data: plot accuracy + confusion matrix
+
+
